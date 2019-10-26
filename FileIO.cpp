29d3091ced35aed48ff1f6a79d5c79ec7c5b9809
guest-2vv3iq <guest-2vv3iq@L221-0000>
2019-10-26 11:14:32 -0400

    Started FileIO (NEED TO TEST)

--- FileIO.cpp
+++ FileIO.cpp
@@ -11,15 +11,51 @@
 #include <string>
 
 using namespace std;
+using namespace KP_FileIO;
 
+//trys to open filename, and return its contents in contents
+//it will strip out only \n's (char return\line feed)
+//returns:
+//SUCCESS all worked, contents string contains all in the file
+//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	ifstream myFile;
+	myFile.open(filename);
+	if (!myFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	string line;
+
+	while (getline(myFile, line)){
+		contents+=line;
+	}
+
+	myFile.close();
+	return SUCCESS;
 }
 
+//trys to open filename, and serialize  myEntryVector contents to it
+//returns:
+//SUCCESS all worked, filename contains myEntryVector strings
+//COULD_NOT_OPEN_FILE_TO_WRITE
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myFile;
+	myFile.open(filename);
+	if (!myFile.is_open()){
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	vector<string>::iterator myStringVectorIterator;
+	for(myStringVectorIterator = myEntryVector.begin();
+			myStringVectorIterator != myEntryVector.end();
+			myStringVectorIterator++){
+		myFile << *myStringVectorIterator + "\n";
+	}
+
+	return SUCCESS;
 }
 
 
